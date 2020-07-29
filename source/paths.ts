9008e3fc03d9f49651e07b13a65d41e7c725ee71
Chroma9 <64189171+chroma9@users.noreply.github.com>
2020-07-29 16:16:02 -0400

    Update paths.ts

--- source/paths.ts
+++ source/paths.ts
@@ -2,6 +2,10 @@ import { join } from 'path';
 
 export const root = join(__dirname, '..');
 
+/**
+ * Path to a file, based on ROOT folder.
+ * @param parts Arguments as strings
+ */
 export function pathTo(...parts: string[]): string {
     return join(root, ...parts);
 }
